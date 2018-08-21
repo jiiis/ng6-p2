1cd68d7c922b457fe166fd27fc24b38605dde1f8
Hao Chang <yngwie.chang@gmail.com>
2018-08-21 23:46:18 +1000

    Added components "user-address" and "user-company".

---
+++ src/app/user-address/user-address.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-address',
+  template: `
+    <p>
+      user-address works!
+    </p>
+  `,
+  styles: []
+})
+export class UserAddressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/user-company/user-company.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-company',
+  template: `
+    <p>
+      user-company works!
+    </p>
+  `,
+  styles: []
+})
+export class UserCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
