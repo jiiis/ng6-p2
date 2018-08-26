7efd15520560596b19310e86a0742c566ad225d0
Hao Chang <yngwie.chang@gmail.com>
2018-08-26 13:25:40 +1000

    Imported module "FormsModule".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule, routingComponents} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
@@ -15,9 +17,10 @@ import {SidebarComponent} from './sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
