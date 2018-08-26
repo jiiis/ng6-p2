d68c41fb9b1cd2687c093cbc4e4f93e20a1ddb5a
Hao Chang <yngwie.chang@gmail.com>
2018-08-26 19:57:02 +1000

    Implemented service "UserService".

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
 import {IUser} from './interfaces';
+import {filter} from 'rxjs/internal/operators';
 
 @Injectable()
 export class UserService {
@@ -8,5 +9,13 @@ export class UserService {
 
   constructor() {}
 
+  get user$() {
+    return this.userSubject.asObservable().pipe(
+      filter((user: IUser) => !!user)
+    );
+  }
 
+  setUser(user: IUser) {
+    this.userSubject.next(user);
+  }
 }
